58c364d0c67d42bdc62b2d65c59650fb3ebc5f04
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-08-19 20:23:36 +0300

    Add nav.js file

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <BrowserRouter>
-      <Navigation />
+      <Nav />
       <Routes>
         <Route path="/" element={<BookList />} />
         <Route path="/categories" element={<Categories />} />
